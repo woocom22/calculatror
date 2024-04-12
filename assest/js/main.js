fbff006ad944d26b0aa2df0f4afe46457315d98c
woocom22 <dkroy2010@outlook.com>
2024-04-12 23:14:02 +0600

    Add filter for number validation, Only input number type date ...

--- assest/js/main.js
+++ assest/js/main.js
@@ -108,6 +108,12 @@ const zero = () =>{
 
 
 
+// This code for number validation
 
 
-
+function isInputNumber(evt){
+    let ch = String.fromCharCode(evt.which);
+    if(!(/[0-9]/.test(ch))){
+        evt.preventDefault();
+    }
+}
