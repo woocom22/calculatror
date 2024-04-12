c976abc5d62175d58bcd3dae919853b234a28210
woocom22 <dkroy2010@outlook.com>
2024-04-12 20:43:10 +0600

    Code added for input number

--- assest/js/main.js
+++ assest/js/main.js
@@ -9,3 +9,105 @@ const backSpeace = () =>{
     const delNum = document.querySelector("#display").value.slice(0, -1);
     document.querySelector("#display").value = delNum;
 }
+
+// Code for input number
+
+const one = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="1";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "1";
+    }
+}
+
+const two = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="2";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "2";
+    }
+}
+
+
+const three = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="3";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "3";
+    }
+}
+
+const four = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="4";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "4";
+    }
+}
+
+
+const five = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="5";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "5";
+    }
+}
+
+const six = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="6";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "6";
+    }
+}
+
+
+const seven = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="7";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "7";
+    }
+}
+
+const eight = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="8";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "8";
+    }
+}
+
+
+const nine = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="9";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "9";
+    }
+}
+
+const zero = () =>{
+    if(document.querySelector("#display").value == " "){
+        document.querySelector("#display").value ="0";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "0";
+    }
+}
+
+
+
+
+
+
