85b8de7a9a9e64b1ec7d017c5a59b8f34ba448ac
woocom22 <dkroy2010@outlook.com>
2024-04-12 19:11:02 +0600

    add code for delete one by one number

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,4 +1,11 @@
 // code for all clear from display
 document.querySelector("#alclear").addEventListener("click", ()=>{
     document.querySelector("#display").value = " ";
-})
+}) 
+
+// code for one by one delete
+
+const backSpeace = () =>{
+    const delNum = document.querySelector("#display").value.slice(0, -1);
+    document.querySelector("#display").value = delNum;
+}
